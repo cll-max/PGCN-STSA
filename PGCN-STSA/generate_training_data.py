c60443a915563a9f3ae1238350d13cfc384bae2d
cll-max <66017184+cll-max@users.noreply.github.com>
2026-03-10 11:57:30 +0800

    Update generate_training_data.py

--- PGCN-STSA/generate_training_data.py
+++ PGCN-STSA/generate_training_data.py
@@ -9,45 +9,6 @@ import os
 import pandas as pd
 
 
-def generate_graph_seq2seq_io_data(
-        df, x_offsets, y_offsets, add_time_in_day=True, add_day_in_week=False, scaler=None
-):
-    """
-    Generate samples from
-    :param df:
-    :param x_offsets:
-    :param y_offsets:
-    :param add_time_in_day:
-    :param add_day_in_week:
-    :param scaler:
-    :return:
-    # x: (epoch_size, input_length, num_nodes, input_dim)
-    # y: (epoch_size, output_length, num_nodes, output_dim)
-    """
-
-    num_samples, num_nodes = df.shape
-    data = np.expand_dims(df.values, axis=-1)
-    feature_list = [data]
-    if add_time_in_day:
-        time_ind = (df.index.values - df.index.values.astype("datetime64[D]")) / np.timedelta64(1, "D")
-        time_in_day = np.tile(time_ind, [1, num_nodes, 1]).transpose((2, 1, 0))
-        feature_list.append(time_in_day)
-    if add_day_in_week:
-        dow = df.index.dayofweek
-        dow_tiled = np.tile(dow, [1, num_nodes, 1]).transpose((2, 1, 0))
-        feature_list.append(dow_tiled)
-
-    data = np.concatenate(feature_list, axis=-1)
-    x, y = [], []
-    min_t = abs(min(x_offsets))
-    max_t = abs(num_samples - abs(max(y_offsets)))  # Exclusive
-    for t in range(min_t, max_t):  # t is the index of the last observation.
-        x.append(data[t + x_offsets, ...])
-        y.append(data[t + y_offsets, ...])
-    x = np.stack(x, axis=0)
-    y = np.stack(y, axis=0)
-    return x, y
-
 
 def generate_train_val_test(args):
     seq_length_x, seq_length_y = args.seq_length_x, args.seq_length_y
@@ -107,3 +68,4 @@ if __name__ == "__main__":
     else:
         os.makedirs(args.output_dir)
     generate_train_val_test(args)
+
